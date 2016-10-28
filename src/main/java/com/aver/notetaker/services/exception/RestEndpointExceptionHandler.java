5315df701258c9affbc987381cd66cb7e602fee2
Mathew Thomas <mattazoid@gmail.com>
2016-10-27 23:52:51 -0400

    removed external log file dependency, cleanp app.yml

--- src/main/java/com/aver/notetaker/services/exception/RestEndpointExceptionHandler.java
+++ src/main/java/com/aver/notetaker/services/exception/RestEndpointExceptionHandler.java
@@ -3,7 +3,7 @@ package com.aver.notetaker.services.exception;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
